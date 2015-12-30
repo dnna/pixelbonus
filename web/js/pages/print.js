749d5cc74872b68dc8ed92a85a2da4a3ce7b5ad4
dnna <dnna@dnna.gr>
2015-12-30 14:50:46 +0200

    Remove print option for #14

--- web/js/pages/print.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $(document).ready(function() {
-        $('#qr-iframe').load(function() {
-            document.getElementById('qr-iframe').contentWindow.print();
-        });
-    });
-});
