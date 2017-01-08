ec574164b837af482eeed4d643c129f766cc6e4c
Dimosthenis Nikoudis <dnna@dnna.gr>
2017-01-08 15:21:36 +0200

    Another 500 error fix in password reset

--- src/Pixelbonus/UserBundle/Controller/ResettingController.php
+++ src/Pixelbonus/UserBundle/Controller/ResettingController.php
@@ -47,7 +47,7 @@ class ResettingController extends BaseResettingController
         $user->setPasswordRequestedAt(new \DateTime());
         $this->container->get('fos_user.user_manager')->updateUser($user);
 
-        return new RedirectResponse($this->generateUrl('fos_user_resetting_check_email',
+        return new RedirectResponse($this->container->get('router')->generateUrl('fos_user_resetting_check_email',
             array('email' => $this->getObfuscatedEmail($user))
         ));
     }
